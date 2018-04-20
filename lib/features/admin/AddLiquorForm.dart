3084bb8fc2ed9b2da2b2d34f1ea1b18701ba6333
Colin <colincmcc@gmail.com>
2018-04-20 16:56:23 -0400

    Continued to add categories to addLiquorForm.  Attempting to create a dropdown textformfield.

--- lib/features/admin/AddLiquorForm.dart
+++ lib/features/admin/AddLiquorForm.dart
@@ -2,6 +2,7 @@ import 'dart:async';
 import 'dart:convert';
 import 'dart:math';
 
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 
@@ -39,14 +40,15 @@ class LiquorFormFieldState extends State<LiquorFormField> {
 
   final GlobalKey<ScaffoldState> _scaffoldKey = new GlobalKey<ScaffoldState>();
   final GlobalKey<FormFieldState<String>> _typeFieldKey = new GlobalKey<FormFieldState<String>>();
-  TextEditingController _textController;
+  TextEditingController _textController = new TextEditingController();
   LiquorData liquor = new LiquorData();
   List<LiquorItem> liquorList = [];
   String _submittedValidation = "";
   final Random _random = new Random();
   List<String> liquorTypes = [];
+  List dropDownFields = [];
+  bool _visible = false;
 
-  String typeFieldText = "";
 
 
 
@@ -149,211 +151,211 @@ class LiquorFormFieldState extends State<LiquorFormField> {
     setState(() {
       liquorTypes = prefs.getStringList("liquorTypes");
       print(liquorTypes);
+      dropDownFields = liquorTypes
+          .map((String value) {
+        return new DropdownMenuItem(
+          value: new Text(value),
+          child: new Text(value),
+        );
+      }
+      ).toList();
+
+      dropDownFields.add(
+        new DropdownMenuItem(
+            value: new Text("Add New"),
+            child: new Text("Add New")
+        )
+      );
     });
 
   }
-  _updateType(){
-    setState((){
 
-    });
-  }
-  @override
-  void initState() {
-    super.initState();
-    _getLiquorTypes();
-    _textController.addListener(_updateType);
-  }
+    @override
+    void initState() {
+      super.initState();
+      _getLiquorTypes();
+    }
 
-  @override
-  Widget build(BuildContext context) {
-    return new Scaffold(
-      key: _scaffoldKey,
-      appBar: new PreferredSize(child: new GradientAppBar("ADD LIQUOR"), preferredSize: const Size.fromHeight(48.0)),
-      body: new SafeArea(
-        top: false,
-        bottom: false,
-        child: new Form(
-          key: _formLiquorKey,
-          autovalidate: _autovalidate,
-          onWillPop: _warnUserAboutInvalidData,
-          child: new SingleChildScrollView(
-            padding: const EdgeInsets.symmetric(horizontal: 16.0),
-            child: new Column(
-              children: <Widget>[
-                new TextFormField(
-                  initialValue: "Clase Azul",
-                  decoration: const InputDecoration(
-                    hintText: 'Name of Liquor',
-                    labelText: 'Name *',
-                  ),
-                  onSaved: (String value) { liquor.name = value; },
-                ),
-            new Row(
-              children: <Widget>[
-                new Expanded(
-                  child: new TextFormField(
-                    controller: _textController,
-                    key: _typeFieldKey,
-                    initialValue: typeFieldText,
-                    decoration: const InputDecoration(
-                      hintText: 'What style of liquor is it?',
-                      labelText: 'Type',
+    @override
+    Widget build(BuildContext context) {
+      return new Scaffold(
+        key: _scaffoldKey,
+        appBar: new PreferredSize(child: new GradientAppBar("ADD LIQUOR"), preferredSize: const Size.fromHeight(48.0)),
+        body: new SafeArea(
+          top: false,
+          bottom: false,
+          child: new Form(
+            key: _formLiquorKey,
+            autovalidate: _autovalidate,
+            onWillPop: _warnUserAboutInvalidData,
+            child: new SingleChildScrollView(
+                padding: const EdgeInsets.symmetric(horizontal: 16.0),
+                child: new Column(
+                  children: <Widget>[
+                    new TextFormField(
+                      initialValue: "Clase Azul",
+                      decoration: const InputDecoration(
+                        hintText: 'Name of Liquor',
+                        labelText: 'Name *',
+                      ),
+                      onSaved: (String value) { liquor.name = value; },
                     ),
-                    onSaved: (String value) { liquor.type = value.toLowerCase(); },
-                  )
-                ),
-                new Expanded(
-                  child: new DropdownButton<String>(
-                    items: liquorTypes
-                        .map((String value) {
-                      return new DropdownMenuItem<String>(
-                        value: value,
-                        child: new Text(value),
-                      );
-                    }
-                    ).toList(),
-                    onChanged: (String value){
-                      setState((){
+                    new DropdownButton(
+                      items: dropDownFields,
+                      hint: new Text("Select a current type or add a new one"),
+                      onChanged: (String value){
                         setState((){
-                          typeFieldText = value;
-                          _textController.clear();
+                          if(value == "Add New") {
+                            _visible = true;
+                          }
+                          else{
+                            _visible = false;
+                            _textController.text = value;
+                          }
                         });
-                      });
-                    },
-                  ),
-                ),
-              ],
+                      },
+                    ),
+                    _visible ? new Container(
+                        child: new TextFormField(
+                          controller: _textController,
+                          decoration: const InputDecoration(
+                            hintText: 'What style of liquor is it?',
+                            labelText: 'Type',
+                          ),
+                          onSaved: (String value) { liquor.type = value.toLowerCase(); },
+                        )
+                    ): new Container(),
+                    new TextFormField(
+                      initialValue: "Clase Azul",
+                      decoration: const InputDecoration(
+                        icon: const Icon(Icons.person),
+                        hintText: 'Who makes this?',
+                        labelText: 'Distillery/Brewery',
+                      ),
+                      onSaved: (String value) { liquor.distillery = value; },
+                    ),
+                    new TextFormField(
+                      initialValue: "https://www.claseazul.com/img/clase-azul-family/plata/tequila-anejo.png",
+                      decoration: const InputDecoration(
+                        hintText: 'Picture of bottle / company logo / etc',
+                        labelText: 'Small logo link',
+                      ),
+                      keyboardType: TextInputType.url,
+                      onSaved: (String value) { liquor.imglink = value; },
+                    ),
+                    new TextFormField(
+                      initialValue: "http://i2.cdn.turner.com/money/dam/assets/170703142445-clase-azul-780x439.jpg",
+                      decoration: const InputDecoration(
+                        hintText: 'Large background image link',
+                        labelText: 'Background image',
+                      ),
+                      keyboardType: TextInputType.url,
+                      onSaved: (String value) { liquor.image = value; },
+                    ),
+                    new TextFormField(
+                      initialValue: "https://www.claseazul.com",
+                      decoration: const InputDecoration(
+                        icon: const Icon(Icons.web),
+                        hintText: 'Link to company or liquor website',
+                        labelText: 'Website',
+                      ),
+                      keyboardType: TextInputType.url,
+                      onSaved: (String value) { liquor.weblink = value; },
+                    ),
+                    new TextFormField(
+                      initialValue: "Clase Azul Añejo “Edición Indígena-Mazahua” (Mazahua Edition) is an ultra- premium añejo tequila made from Tequilana Weber Blue Agave. Its intense amber color and layered aromas are a result of an extended period of aging.",
+                      decoration: const InputDecoration(
+                        hintText: 'Tell us about the liquor',
+                        helperText: 'Keep it short',
+                        labelText: 'About',
+                      ),
+                      onSaved: (String value) { liquor.description = value; },
+                      maxLines: 5,
+                    ),
+                    new TextFormField(
+                      initialValue: "34",
+                      keyboardType: TextInputType.number,
+                      decoration: const InputDecoration(
+                          labelText: 'Price',
+                          prefixText: '\$',
+                          suffixStyle: const TextStyle(color: Colors.green)
+                      ),
+                      onSaved: (String value) { liquor.price = value; },
+                      maxLines: 1,
+                    ),
+                    new TextFormField(
+                      initialValue: "110",
+                      keyboardType: TextInputType.number,
+                      decoration: const InputDecoration(
+                        labelText: 'Proof',
+                      ),
+                      onSaved: (String value) { liquor.proof = value; },
+                      maxLines: 1,
+                    ),
+                    new TextFormField(
+                      initialValue: "3 Years",
+                      keyboardType: TextInputType.text,
+                      decoration: const InputDecoration(
+                        labelText: 'Age',
+                        helperText: 'Please specify if years/months',
+                      ),
+                      onSaved: (String value) { liquor.age = value; },
+                      maxLines: 1,
+                    ),
+                    new TextFormField(
+                      initialValue: "Tequila",
+                      keyboardType: TextInputType.text,
+                      decoration: const InputDecoration(
+                        labelText: 'Substyle',
+                        hintText: 'Stout / Barrel Aged / etc.',
+                      ),
+                      onSaved: (String value) { liquor.style = value; },
+                      maxLines: 1,
+                    ),
+                    new TextFormField(
+                      initialValue: "TASTE: Cooked agave flavors with touches of vanilla, caramel, and various woods through the finish.",
+                      keyboardType: TextInputType.text,
+                      decoration: const InputDecoration(
+                        labelText: 'Bill/Tasting Notes',
+                      ),
+                      onSaved: (String value) { liquor.bill = value; },
+                      maxLines: 1,
+                    ),
+                    new TextFormField(
+                      initialValue: "Intense Amber",
+
+                      keyboardType: TextInputType.text,
+                      decoration: const InputDecoration(
+                        labelText: 'Color',
+                      ),
+                      onSaved: (String value) { liquor.color = value; },
+                      maxLines: 1,
+                    ),
+                    new Container(
+                      padding: const EdgeInsets.all(20.0),
+                      alignment: Alignment.center,
+                      child: new RaisedButton(
+                        child: const Text('SUBMIT'),
+                        onPressed: _handleSubmitted,
+                      ),
+                    ),
+                    new Container(
+                        child: new Text(_submittedValidation)
+                    ),
+                    new Container(
+                      padding: const EdgeInsets.only(top: 20.0),
+                      child: new Text('* indicates required field', style: Theme.of(context).textTheme.caption),
+                    ),
+
+                  ],
+                )
+
+
             ),
-                new TextFormField(
-                  initialValue: "Clase Azul",
-                  decoration: const InputDecoration(
-                    icon: const Icon(Icons.person),
-                    hintText: 'Who makes this?',
-                    labelText: 'Distillery/Brewery',
-                  ),
-                  onSaved: (String value) { liquor.distillery = value; },
-                ),
-                new TextFormField(
-                  initialValue: "https://www.claseazul.com/img/clase-azul-family/plata/tequila-anejo.png",
-                  decoration: const InputDecoration(
-                    hintText: 'Picture of bottle / company logo / etc',
-                    labelText: 'Small logo link',
-                  ),
-                  keyboardType: TextInputType.url,
-                  onSaved: (String value) { liquor.imglink = value; },
-                ),
-                new TextFormField(
-                  initialValue: "http://i2.cdn.turner.com/money/dam/assets/170703142445-clase-azul-780x439.jpg",
-                  decoration: const InputDecoration(
-                    hintText: 'Large background image link',
-                    labelText: 'Background image',
-                  ),
-                  keyboardType: TextInputType.url,
-                  onSaved: (String value) { liquor.image = value; },
-                ),
-                new TextFormField(
-                  initialValue: "https://www.claseazul.com",
-                  decoration: const InputDecoration(
-                    icon: const Icon(Icons.web),
-                    hintText: 'Link to company or liquor website',
-                    labelText: 'Website',
-                  ),
-                  keyboardType: TextInputType.url,
-                  onSaved: (String value) { liquor.weblink = value; },
-                ),
-                new TextFormField(
-                  initialValue: "Clase Azul Añejo “Edición Indígena-Mazahua” (Mazahua Edition) is an ultra- premium añejo tequila made from Tequilana Weber Blue Agave. Its intense amber color and layered aromas are a result of an extended period of aging.",
-                  decoration: const InputDecoration(
-                    hintText: 'Tell us about the liquor',
-                    helperText: 'Keep it short',
-                    labelText: 'About',
-                  ),
-                  onSaved: (String value) { liquor.description = value; },
-                  maxLines: 5,
-                ),
-                new TextFormField(
-                  initialValue: "34",
-                  keyboardType: TextInputType.number,
-                  decoration: const InputDecoration(
-                      labelText: 'Price',
-                      prefixText: '\$',
-                      suffixStyle: const TextStyle(color: Colors.green)
-                  ),
-                  onSaved: (String value) { liquor.price = value; },
-                  maxLines: 1,
-                ),
-                new TextFormField(
-                  initialValue: "110",
-                  keyboardType: TextInputType.number,
-                  decoration: const InputDecoration(
-                    labelText: 'Proof',
-                  ),
-                  onSaved: (String value) { liquor.proof = value; },
-                  maxLines: 1,
-                ),
-                new TextFormField(
-                  initialValue: "3 Years",
-                  keyboardType: TextInputType.text,
-                  decoration: const InputDecoration(
-                    labelText: 'Age',
-                    helperText: 'Please specify if years/months',
-                  ),
-                  onSaved: (String value) { liquor.age = value; },
-                  maxLines: 1,
-                ),
-                new TextFormField(
-                  initialValue: "Tequila",
-                  keyboardType: TextInputType.text,
-                  decoration: const InputDecoration(
-                    labelText: 'Substyle',
-                    hintText: 'Stout / Barrel Aged / etc.',
-                  ),
-                  onSaved: (String value) { liquor.style = value; },
-                  maxLines: 1,
-                ),
-                new TextFormField(
-                  initialValue: "TASTE: Cooked agave flavors with touches of vanilla, caramel, and various woods through the finish.",
-                  keyboardType: TextInputType.text,
-                  decoration: const InputDecoration(
-                    labelText: 'Bill/Tasting Notes',
-                  ),
-                  onSaved: (String value) { liquor.bill = value; },
-                  maxLines: 1,
-                ),
-                new TextFormField(
-                  initialValue: "Intense Amber",
-
-                  keyboardType: TextInputType.text,
-                  decoration: const InputDecoration(
-                    labelText: 'Color',
-                  ),
-                  onSaved: (String value) { liquor.color = value; },
-                  maxLines: 1,
-                ),
-                new Container(
-                  padding: const EdgeInsets.all(20.0),
-                  alignment: Alignment.center,
-                  child: new RaisedButton(
-                    child: const Text('SUBMIT'),
-                    onPressed: _handleSubmitted,
-                  ),
-                ),
-                new Container(
-                    child: new Text(_submittedValidation)
-                ),
-                new Container(
-                  padding: const EdgeInsets.only(top: 20.0),
-                  child: new Text('* indicates required field', style: Theme.of(context).textTheme.caption),
-                ),
-
-              ],
-            )
-
-
           ),
         ),
-      ),
-    );
+      );
+    }
   }
 }
 
